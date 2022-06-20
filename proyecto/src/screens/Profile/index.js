45beb617d5fc3b9d0b55df2dd2ac20a3cba05280
alecassera <acassera@udesa.edu.ar>
2022-06-20 19:47:07 -0300

    css profille

--- proyecto/src/screens/Profile/index.js
+++ proyecto/src/screens/Profile/index.js
@@ -1,8 +1,10 @@
 import React, {Component} from "react";
 import {View, Text, TouchableOpacity, FlatList, Image, StyleSheet} from "react-native"
-import {db, auth} from "../../firebase/config"
 import Post from "../../components/Post"
+
 import { Feather } from '@expo/vector-icons';
+
+import {db, auth} from "../../firebase/config"
 
 class Profile extends Component{
     constructor(props){
@@ -12,11 +14,8 @@ class Profile extends Component{
             loading: true,
             userInfo: {},
             display: "grid"
-
         }
     }
-
-  
 
     componentDidMount(){
         db.collection("posts").where("useremail", "==", auth.currentUser.email).onSnapshot( //todo lo que trae onSnapshot lo trae en forma de array
@@ -60,53 +59,74 @@ class Profile extends Component{
         }
     }
 
+    sinceLastLogin(){
+        let previousLogin = auth.currentUser.metadata.lastSignInTime
+        let actualLogin = parseInt(Date.now())
+
+        // -- Math.trunc() se queda con la parte entera de la division -- //
+        let seconds = Math.trunc(actualLogin / 1000) 
+        let minutes = Math.trunc(seconds / 60)
+        let hours = Math.trunc(minutes / 60)
+        let days = Math.trunc(hours / 24)
+        return days
+    }
+
     render() {
         console.log(this.state.userPosts)
-        console.log(this.state.posts)
+        console.log(auth.currentUser.metadata.lastSignInTime)
         return (
             <View style={styles.screen}>
 
-            <View>
-                <Text>
-                    @{this.state.userInfo.username}
-                </Text>
-                <Text>
-                    {auth.currentUser.metadata.lastSignInTime}
-                    {auth.currentUser.metadata.creationTime}
-                </Text>
-            </View>
+                <View style={styles.userInfoContainer}>
+                    <Text style={styles.userName}>
+                        @{this.state.userInfo.username}
+                    </Text>
+                    <Text>
+                        {auth.currentUser.metadata.lastSignInTime}
+                        {auth.currentUser.metadata.creationTime}
+                    </Text>
+                    <Text>
+                        {this.state.userPosts.length}
+                    </Text>
+                </View>
 
-            <TouchableOpacity onPress={() => this.changeDisplay()}>
-                {this.state.display === 'grid' ?
-                        <Feather name="menu" size={24} color="white" /> :
-                        <Feather name="grid" size={24} color="white" />
-                }
-            </TouchableOpacity>
-
-               
-                <View style={styles.prueba}>
+                <View style={styles.displayContainer}>
+                    <TouchableOpacity onPress={() => this.changeDisplay()}>
+                        {this.state.display === 'grid' ?
+                                <Feather name="menu" size={24} color="white" /> :
+                                <Feather name="grid" size={24} color="white" />
+                        }
+                    </TouchableOpacity>
+                </View>
+                
+                <View style={styles.flatListContainer}>
                     { this.state.display === 'grid' ?
-                    <FlatList
-                        data={this.state.userPosts}
-                        key={'g'}
-                        numColumns={3}
-                        keyExtractor={item => item.id.toString()} 
-                        renderItem ={({item}) =>
-                            <Image
-                                source={{uri: item.data.photo}}
-                                resizeMode="cover"
-                                style={styles.image}
-                            />                             
-                        }
-                    /> :
-                    <FlatList
-                        data={this.state.userPosts}
-                        key={'c'}
-                        keyExtractor={item => item.id.toString()} 
-                        renderItem ={({item}) =>
-                            <Post navigation={this.props.navigation} postInfo={item} styles={styles}/>
-                        }
-                    />
+                    <View style={styles.flatListGridContainer}>
+                        <FlatList
+                            style={styles.flatListGrid}
+                            data={this.state.userPosts}
+                            key={'g'}
+                            numColumns={3}
+                            keyExtractor={item => item.id.toString()} 
+                            renderItem ={({item}) =>
+                                <Image
+                                    source={{uri: item.data.photo}}
+                                    resizeMode="cover"
+                                    style={styles.image}
+                                />                             
+                            }
+                        /> 
+                    </View>:
+                    <View style={styles.flatListColumnContainer}>
+                        <FlatList
+                            data={this.state.userPosts}
+                            key={'c'}
+                            keyExtractor={item => item.id.toString()} 
+                            renderItem ={({item}) =>
+                                <Post navigation={this.props.navigation} postInfo={item} styles={styles}/>
+                            }
+                        />
+                    </View>
                     }
                 </View>
 
@@ -122,12 +142,46 @@ class Profile extends Component{
 export default Profile;
 
 const styles = StyleSheet.create({
-    image: {
-        width: 135,
-        height: 135,
+    userInfoContainer: {
+        backgroundColor: "pink",
+        flex: 2,
+        padding: 10
     },
+    userName: {
+        color: 'white',
+        display: 'flex',
+        alignItems: 'left',
+        fontSize: 17,
+        fontWeight: 500,
+        display: 'flex',
+        flexDirection: 'column'
+    },
+
+    displayContainer: {
+        flex: 1
+    },
+
+    flatListContainer: {
+        flex: 10
+    },
+
+    flatListGridContainer: {
+        flex: 1, 
+        backgroundColor: "yellow"
+    },
+
+    flatListColumnContainer: {
+        flex: 1
+    },
+
+    image: {
+        width: "33.3333%",
+        height: 200,
+        padding: 1
+    },
+
     screen: {
         backgroundColor: '#202020',
-        height: '100%'
+        flex: 1
       },
 })
